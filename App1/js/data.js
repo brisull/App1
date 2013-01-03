8a3e963e3908dd1be0dbf3ff54c44ff412a5f834
brisull <bri_sull@yahoo.com>
2013-01-03 07:20:23 -0800

    initial commit

---
+++ App1/js/data.js
@@ -0,0 +1,38 @@
+﻿(function () {
+    "use strict";
+
+
+    var _baseUrl = "http://ws.spotify.com/search/1/track.json";
+    var _query = "the+clash";
+    var _list = new WinJS.Binding.List();
+
+    WinJS.Namespace.define("Spotify", {
+        search: searchSpotify,
+        list: _list,
+        foo: "bar"
+    });
+
+    function searchSpotify(query) {
+        if ( !(query )) {
+            query = _query;
+        }
+        _list.splice(0,_list.length);
+        var url = _baseUrl + "?q=" + encodeURIComponent(query);
+        console.log("searching for: " + url);
+        return WinJS.xhr({ url: url }).then( 
+            function onSuccess (xhr) {
+                var items = JSON.parse(xhr.responseText);
+                // console.dir(items.tracks);
+                items.tracks.forEach(function (item) {
+                    _list.push(item);
+                });
+            },
+            function onError(error) {
+                console.log(error);
+            })
+
+        
+    }
+
+
+})();
---
+++ App1/pages/home/home.js
@@ -0,0 +1,46 @@
+﻿(function () {
+    "use strict";
+
+    var ui = WinJS.UI;
+
+    
+
+    WinJS.UI.Pages.define("/pages/home/home.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            // TODO: Initialize the page here.
+            //var renderElement = document.getElementById("templateControlRenderTarget");
+            var templateElement = document.getElementById("templateDiv");
+            var templateControl = templateElement.winControl;
+            
+            document.getElementById("search").innerHTML = "foo";
+
+            function search(query) {
+                Spotify.search(query).then(function (response) {
+                    templateControl.layout = new ui.GridLayout({ groupHeaderPosition: "top" });
+                    // console.log(Spotify.foo);
+                    //var i = 0;
+                    //Spotify.list.forEach(function (item) {
+                    //    if (i > 15) {
+                    //        return false;
+                    //    }
+                    //    i++;
+                    //    // document.getElementById("debug").innerHTML += item.name + "<br />";
+                    //    templateControl.render(item, renderElement);
+
+                    //});
+                });
+            }
+
+            
+            //templateElement.winControl.render(people[selected--], renderElement);
+            
+
+            document.getElementById("spotSearch").addEventListener("change", function(e) {
+                // console.log("Search for: " + e.target.value);
+                search(e.target.value);
+            });
+        }
+    });
+})();
